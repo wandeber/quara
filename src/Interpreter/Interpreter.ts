5fdac59c238c35b0109b69dc209d5544b7ffc43e
wandeber <berthewanderer@gmail.com>
2024-04-24 00:34:55 +0200

    Add documentation for new standard functions.

--- src/Interpreter/Interpreter.ts
+++ src/Interpreter/Interpreter.ts
@@ -40,8 +40,7 @@ import ASTArray from "../ASTNodes/ASTArray";
 import ASTArrayVisitor from "./ASTArrayVisitor";
 
 
-
-
+const Documentation = new Map();
 
 const DefaultVariables = {
   contains(arr: string|any[], ...args: [any, any]) {
@@ -69,7 +68,7 @@ const DefaultVariables = {
   },
 
   // Math:
-  abs(num: number) {
+  abs(num: number): number {
     return Math.abs(num);
   },
   ceil(num: number, decimals = 0) {
@@ -117,16 +116,123 @@ const DefaultVariables = {
     return Array.isArray(arr);
   },
 
-  /*
   help(ptr: any) {
-    console.log(ptr);
+    console.log(Documentation.get(ptr));
   },
-  */
-  print(arg: any) {
+
+  log(arg: any) {
     console.log(arg);
+  },
+
+  print(arg: any) {
+    return arg;
+  },
+
+  hi() {
+    console.log("Hello, World!");
+    return "Hello, World!";
   },
 };
 
+Documentation.set(
+  DefaultVariables.contains,
+  "contains(arr: string|any[], ...args: [any, any]): boolean\n  Returns true if the array contains the element.",
+);
+Documentation.set(
+  DefaultVariables.isNaN,
+  "isNaN(num: number): boolean\n  Returns true if the argument is NaN.",
+);
+
+Documentation.set(
+  DefaultVariables.length,
+  "length(str: string): number\n  Returns the length of a string.",
+);
+Documentation.set(
+  DefaultVariables.split,
+  "split(str: string, separator: string): string[]\n  Splits a string into an array of substrings.",
+);
+Documentation.set(
+  DefaultVariables.join,
+  "join(arr: (string|number)[], separator = ''): string\n  Joins all elements of an array into a string.",
+);
+Documentation.set(
+  DefaultVariables.upperCase,
+  "upperCase(str: string): string\n  Converts a string to uppercase.",
+);
+Documentation.set(
+  DefaultVariables.lowerCase,
+  "lowerCase(str: string): string\n  Converts a string to lowercase.",
+);
+
+Documentation.set(DefaultVariables.abs, "abs(num: number): number\n  Returns the absolute value of a number.");
+Documentation.set(
+  DefaultVariables.ceil,
+  "ceil(num: number, decimals = 0): number\n  Returns the smallest integer greater than or equal to a number.",
+);
+Documentation.set(
+  DefaultVariables.floor,
+  "floor(num: number, decimals = 0): number\n  Returns the largest integer less than or equal to a number.",
+);
+Documentation.set(
+  DefaultVariables.round,
+  "round(num: number, decimals = 0): number\n  Returns the value of a number rounded to the nearest integer.",
+);
+Documentation.set(
+  DefaultVariables.trunc,
+  "trunc(num: number, decimals = 0): number\n  Returns the integer part of a number by removing any fractional digits.",
+);
+Documentation.set(
+  DefaultVariables.max,
+  "max(...args: number[]): number\n  Returns the largest of zero or more numbers.",
+);
+Documentation.set(
+  DefaultVariables.min,
+  "min(...args: number[]): number\n  Returns the smallest of zero or more numbers.",
+);
+Documentation.set(
+  DefaultVariables.fixed,
+  "fixed(num: number, ...args: any[]): string\n  Formats a number using fixed-point notation.",
+);
+Documentation.set(
+  DefaultVariables.isNumber,
+  "isNumber(num: any): boolean\n  Returns true if the argument is a number.",
+);
+Documentation.set(
+  DefaultVariables.isDecimal,
+  "isDecimal(num: any): boolean\n  Returns true if the argument is a decimal number.",
+);
+Documentation.set(
+  DefaultVariables.isInteger,
+  "isInteger(num: any): boolean\n  Returns true if the argument is an integer number.",
+);
+Documentation.set(
+  DefaultVariables.isString,
+  "isString(str: any): boolean\n  Returns true if the argument is a string.",
+);
+Documentation.set(
+  DefaultVariables.isBoolean,
+  "isBoolean(bool: any): boolean\n  Returns true if the argument is a boolean.",
+);
+Documentation.set(
+  DefaultVariables.isObject,
+  "isObject(obj: any): boolean\n  Returns true if the argument is an object.",
+);
+Documentation.set(
+  DefaultVariables.isArray,
+  "isArray(arr: any): boolean\n  Returns true if the argument is an array.",
+);
+Documentation.set(
+  DefaultVariables.help,
+  "help(ptr: any): void\n  Shows the documentation of a function.",
+);
+Documentation.set(
+  DefaultVariables.log,
+  "log(arg: any): void\n  Logs a message to the console.",
+);
+Documentation.set(
+  DefaultVariables.print,
+  "print(arg: any): any\n  Prints the argument.",
+);
 
 export default class Interpreter extends ASTInterpreter {
   parser: Parser;
